9eca5e2932ff21288d80dd61cb68d973b7aaea79
Iain Ballard <iain.ballard@ewater.services>
2023-02-15 10:37:53 +0000

    more tests

--- OcvFrames/FfmpegFileSynthesiser.cs
+++ OcvFrames/FfmpegFileSynthesiser.cs
@@ -38,7 +38,7 @@ public class FfmpegFileSynthesiser : IDisposable
         _frameImage = new Bitmap(width, height, PixelFormat.Format24bppRgb);
     }
         
-    IEnumerable<IVideoFrame> CreateFrames()
+    IEnumerable<IVideoFrame> CreateVideoFrames()
     {
         if (_source is null) yield break;
         
@@ -53,10 +53,11 @@ public class FfmpegFileSynthesiser : IDisposable
             more = _source.DrawFrame(i++, g);
             var frame = new BitmapVideoFrameWrapper(_frameImage); // we do NOT dispose of this frame, as that kills the source bitmap and nothing else
             yield return frame;
+            //Console.Write('v');
         }
     }
 
-    private IEnumerator<IAudioSample> CreateAudio()
+    private IEnumerator<IAudioSample> CreateAudioFrames()
     {
         var toneSamples = 44100 / 440;
         var sample = new byte[toneSamples];
@@ -64,27 +65,28 @@ public class FfmpegFileSynthesiser : IDisposable
 
         for (int i = 0; i < toneSamples; i++)
         {
-            sample[i] = (byte)((Math.Sin(i+r) * 64) + 127);
+            sample[i] = (byte)((Math.Sin(i*r) * 32.0) + 127.0);
         }
         
         Console.WriteLine("First audio frame requested");
         
-        for (int i = 0; i < 440; i++) // one second of audio
+        for (int i = 0; i < 440 * 10; i++) // 10 seconds of audio (video is only 10 seconds)
         {
             yield return new PcmAudioSampleWrapper(sample);
+            //Console.Write('a');
         }
     }
 
     public void WriteVideo(IVideoGenerator source)
     {
         _source = source;
-        var videoFramesSource = new RawVideoPipeSource(CreateFrames())
+        var videoFramesSource = new RawVideoPipeSource(CreateVideoFrames())
         {
             FrameRate = _framesPerSecond
         };
         
         // https://trac.ffmpeg.org/wiki/audio%20types
-        var audioSource = new RawAudioPipeSource(CreateAudio()){
+        var audioSource = new RawAudioPipeSource(CreateAudioFrames()){
             Channels = 1, SampleRate = 44100, Format = "u8" // PCM unsigned 8-bit
         };
             
